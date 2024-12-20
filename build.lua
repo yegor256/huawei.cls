7cd50a5e9d4045a3c76e802aa7bc4cd3e23ead1c
Yegor Bugayenko <yegor256@gmail.com>
2024-12-20 11:00:43 +0300

    date format

--- build.lua
+++ build.lua
@@ -52,6 +52,6 @@ uploadconfig = {
 function update_tag (file , content , tagname , tagdate)
   return string.gsub(
     string.gsub(content, "0%.0%.0", tagname),
-    "0000%-00%-00", os.date("%Y-%m-%d")
+    "0000/00/00", os.date("%Y/%m/%d")
   )
 end
